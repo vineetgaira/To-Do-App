dbe6ba7b69b17cc99e3160831e84e023030d2485
Vineet Gaira <vineetgaira1729@gmail.com>
2026-08-14 13:11:38 +0000

    Create menu

--- todo/menu.py
+++ todo/menu.py
@@ -8,6 +8,13 @@ def menu() -> str:
     print(Fore.GREEN + "              OPTIONS")
     print(Fore.MAGENTA + "="*36)
 
+    print(Fore.WHITE + " [1] " +  Fore.LIGHTCYAN_EX + "CREATE TASK")
+    print(Fore.WHITE + " [2] " +  Fore.LIGHTCYAN_EX + "VIEW TASK")
+    print(Fore.WHITE + " [3] " +  Fore.LIGHTCYAN_EX + "COMPLETE TASK")
+    print(Fore.WHITE + " [4] " +  Fore.LIGHTCYAN_EX + "UNCOMPLETE TASK")
+    print(Fore.WHITE + " [5] " +  Fore.LIGHTCYAN_EX + "EDIT TASK")
+    print(Fore.WHITE + " [6] " +  Fore.LIGHTCYAN_EX + "DELETE TASK")
+
 # Create task  
 # View tasks
 # Complete task
