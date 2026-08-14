ae36c7da5ec8ff8148b611a7acdcad5cc61b587d
Vineet Gaira <vineetgaira1729@gmail.com>
2026-08-14 13:13:51 +0000

    Created menu removed main loop

--- todo/menu.py
+++ todo/menu.py
@@ -13,13 +13,7 @@ def menu() -> str:
     print(Fore.WHITE + " [3] " +  Fore.LIGHTCYAN_EX + "COMPLETE TASK")
     print(Fore.WHITE + " [4] " +  Fore.LIGHTCYAN_EX + "UNCOMPLETE TASK")
     print(Fore.WHITE + " [5] " +  Fore.LIGHTCYAN_EX + "EDIT TASK")
-    print(Fore.WHITE + " [6] " +  Fore.LIGHTCYAN_EX + "DELETE TASK")
-
-# Create task  
-# View tasks
-# Complete task
-# Uncomplete task
-# Edit task
-# Delete task
+    print(Fore.WHITE + " [6] " +  Fore.LIGHTCYAN_EX + "DELETE TASK") 
+    print(Fore.WHITE + " [7] " +  Fore.LIGHTCYAN_EX + "EXIT") 
 
 menu()
--- todo/task.py
+++ todo/task.py
@@ -25,31 +25,4 @@ class Task:
         return f"[{status}] {self.task_id} - {self.title} - {self.description}"
     
 
-while True: 
-    print(Fore.BLUE + Style.BRIGHT + "Press 1 to write new tasks." + Style.RESET_ALL)
-    print(Fore.BLUE + Style.BRIGHT + "Press 2 to see and mark tasks." + Style.RESET_ALL)
-    choice = int(input(Fore.BLUE + "Enter your choice: "))
-    if choice == 1:
-        task_id = random.randint(1,1001)
-        title = input(Fore.BLUE + "Enter title of the task: ")
-        description = input(Fore.BLUE + "Enter description of the task: ")
-        task = Task(task_id, title, description)
-        ask_exit = input(Fore.BLUE + "Do you wanna add one more task? y/n: ").strip().lower()
-        if ask_exit == "y":
-            continue
-        elif ask_exit == "n":
-            break
-        else:
-            print(Fore.RED + "Please enter 'y/n'.")
-    elif choice == 2:
-        print(Fore.LIGHTCYAN_EX + task)
-        choice = input(Fore.BLUE + "Have you completed the task? y/n: ").strip().lower()
-        if choice == "y":
-            task.complete()
-            print(Fore.LIGHTCYAN_EX + task)
-        elif ask_exit == "n":
-            task.uncomplete()
-            print(Fore.LIGHTCYAN_EX + task)
-        else:
-            break
 
