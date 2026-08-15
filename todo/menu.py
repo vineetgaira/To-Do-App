6108a5c3cf17da412b77c97fc4f478f083377166
Vineet Gaira <vineetgaira1729@gmail.com>
2026-08-15 12:44:56 +0000

    commit changes

--- todo/menu.py
+++ todo/menu.py
@@ -27,4 +27,3 @@ def menu() -> str:
     print(Fore.WHITE + " [5] " +  Fore.LIGHTCYAN_EX + "EDIT TASK")
     print(Fore.WHITE + " [6] " +  Fore.LIGHTCYAN_EX + "DELETE TASK") 
     print(Fore.WHITE + " [7] " +  Fore.LIGHTCYAN_EX + "EXIT") 
-
