418eb56b53f158c7feb8c566e6d2fa1cc58005e3
Vineet Gaira <vineetgaira1729@gmail.com>
2026-09-01 05:10:11 +0000

    A basic sekeleton is done

--- todo/task.py
+++ todo/task.py
@@ -30,3 +30,56 @@ Eventually you'll have:
     Storage
 This is where everything you've learned starts coming together.
 """
+
+class Task:
+
+    total_tasks = 0
+
+    def __init__(self, task_id, title, description):
+        self.task_id = task_id
+        self.title = title
+        self.description = description
+
+        Task.total_tasks += 1
+
+    def get_task(self):
+        print(f"Task ID: {self.task_id}")
+        print(f"Title: {self.title}")
+        print(f"Description: {self.description}\n")
+class TaskManager:
+
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+
+        self.tasks.append(task)
+        print(f"Added: {task.title}/{task.task_id}")
+
+    def find_task(self, task_id):
+
+        task = None
+
+        for t in self.tasks:
+            if t.task_id == task_id:
+                task = t
+                break
+
+        return task
+
+
+    def remove_task(self, task_id):
+        task = self.find_task(task_id)
+
+        if task is None:
+            print(f"No task found with Task ID: {task_id}")
+        else:
+            self.tasks.remove(task)
+            print(f"Removed: {task.title}/{task.task_id}")
+
+task = Task("101", "Morning Run", "Run 5km in 18 minutes in the morning.")
+manager = TaskManager()
+
+manager.add_task(task)
+task.get_task()
+manager.remove_task("301")
