6d3700c0c76e976e86dae59bb35461ade842afa3
Vineet Gaira <vineetgaira1729@gmail.com>
2026-09-10 06:39:18 +0000

    Added comments

--- todo/storage.py
+++ todo/storage.py
@@ -5,14 +5,16 @@ from todo.task import Task
 
 class JSONStorage:
 
+    # We create a constructor to create filepath
     def __init__(self, filepath):
         self.filepath = Path(filepath)
 
+    # This file saves data to json
     def save(self, tasks):
         data = [task.to_dict() for task in tasks]
         with open (self.filepath, "w") as f:
             json.dump(data, f, indent=4)
-
+    # This file loads data from json file
     def load(self):
         try:
             with open(self.filepath, "r") as f:
@@ -22,19 +24,24 @@ class JSONStorage:
 
         return [Task.from_dict(i) for i in data]
 
+
+# This is another class for the purpose of CSV storage
 class CSVStorage:
 
+    # Here it takes the same filepath and creates title for them
     def __init__(self, filepath):
         self.filepath = Path(filepath)
         self.fieldnames = ["task_id", "title", "description", "priority", "status", "due_date"]
 
-
+    # This saves tasks to csv
     def save(self, tasks):
         with open(self.filepath, "w", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=self.fieldnames)
             writer.writeheader()
             for task in tasks:
                 writer.writerow(task.to_dict())
+
+    # This loads tasks from csv      
     def load(self):
         try:
             with open(self.filepath, "r", newline="") as f:
